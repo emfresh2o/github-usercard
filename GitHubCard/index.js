3941e1ca15e09a0adff3f09516bf08dc463c2086
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-07-16 16:37:36 -0400

    first commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,17 @@
+import Axios from "axios";
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+let cards = document.querySelector('.card');
+axios.get ("https://api.github.com/users/emfresh2o").then (response => {
+  cards.appendChild(GitHubCard(response.data));
+})
+.catch(error => {
+  console.log(error);
+})
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
